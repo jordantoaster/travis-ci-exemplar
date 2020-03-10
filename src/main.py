ec0f139ddfc61fe55db6d8a610ad2875ed7a8e93
jordantoaster <jordanmcdonaldmain@gmail.com>
2020-03-10 13:11:43 +0000

    example function, example text, travis CI, make file.

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def main() -> list:
+    """
+    Function Implementation is unimportant for this repository.
+    """
+    return np.random.random_sample(10)
+
+# Entry point to execute Python Code.
+print(main())
